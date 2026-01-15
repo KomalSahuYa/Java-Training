d0732eb72478f5bef351d3a8a1cf87468333967b
Komal Sahu <VE00YN373@YMI.COM>
2026-01-15 13:27:58 +0530

    first commit

---
+++ Day1_java/src/question4/PascalTriangle.java
@@ -0,0 +1,31 @@
+package question4;
+
+import java.util.Scanner;
+
+public class PascalTriangle {
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter the size of the array");
+			int n = sc.nextInt();
+			int pas[][] = new int[n][];
+			pas[0] = new int[1];
+			pas[0][0] = 1;
+			for (int i = 0; i < n; i++) {
+				pas[i] = new int[i + 1];
+				pas[i][0] = 1;
+				pas[i][i] = 1;
+				for (int j = 1; j < i; j++) {
+					pas[i][j] = pas[i - 1][j - 1] + pas[i - 1][j];
+				}
+			}
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < pas[i].length; j++) {
+					System.out.print(pas[i][j] + " ");
+				}
+				System.out.println();
+
+			}
+
+		}
+	}
+}
